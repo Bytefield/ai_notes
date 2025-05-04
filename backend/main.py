df62f807b23d402fe044a58f7fea452388f0db9b
Bytefield <ramosperez1978@gmail.com>
2025-05-04 09:25:15 +0200

    remove: duplicated files out of correct folder

--- backend/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI, Request
-import httpx
-
-app: FastAPI = FastAPI()
-
-
-@app.api_route("full_path/path", methods=["GET", "POST", "PUT", "DELETE"])
-async def proxy(reqquest: Request, full_path: str) -> dict[str, str]:
-    return {"message": "Proxy route"}
